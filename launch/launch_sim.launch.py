8adabcee622008581a845f410b84cc8f070bca5c
synguyen25 <synguyenfa@gmail.com>
2023-10-31 02:57:04 -0700

    initial commit

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -22,21 +22,23 @@ def generate_launch_description():
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control':'true'}.items()
     )
-    gazebo_params_file=os.path.join(get_package_share_directory('gazebo_ros'), 'config', 'gazebo_params.yaml')
+    gazebo_params_path = os.path.join(
+                  get_package_share_directory(package_name),'config','gazebo_params.yaml')
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-                    launch_arguments={'extra_gazebo_args': '--ros-arg --params-file' + gazebo_params_file}.items()
+                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_path }.items()
              )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
-                                   '-entity', 'ute_datn'],
+                                   '-entity', 'my_bot'],
                         output='screen')
+
 
     diff_drive_spawner = Node(
         package="controller_manager",
@@ -50,6 +52,8 @@ def generate_launch_description():
         arguments=["joint_broad"],
     )
 
+
+
     # Launch them all!
     return LaunchDescription([
         rsp,
@@ -57,4 +61,5 @@ def generate_launch_description():
         spawn_entity,
         diff_drive_spawner,
         joint_broad_spawner
+
     ])
